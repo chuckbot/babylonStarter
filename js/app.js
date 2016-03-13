332941c3e28ed47487ae42b3d8ca6fbe5ed1a295
chuckbot <cgmilan@gmail.com>
2016-03-12 23:28:55 -0430

    Se creo un piso y una segunda esfera

--- js/app.js
+++ js/app.js
@@ -15,12 +15,20 @@ BjsApp.init = function(){
     //Crear camara
     var camera = new BABYLON.FreeCamera('FreeCamera', new BABYLON.Vector3(0, 2, -15), scene);
     
+    camera.attachControl(canvas);
+    
     //Luz de ambiente
     var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+    
+    var ground = BABYLON.Mesh.CreateGround('ground1', 20, 20, 2, scene);
     
     //crear un esfera
     var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
     sphere.position.y = 1;
+    
+    var sphere2 = BABYLON.Mesh.CreateSphere('sphere2', 16, 4, scene);
+    sphere2.position = new BABYLON.Vector3(3, 3, 3);
+    
     
     engine.runRenderLoop(function(){
         scene.render();
